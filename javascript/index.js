b6fc78c39d9fccbfb6e194e48730cb8b7670ce8f
adayperez8-glitch <adayperez8@gmail.com>
2026-03-26 16:03:02 +0100

    promises terminado

--- javascript/index.js
+++ javascript/index.js
@@ -29,12 +29,78 @@
 
 // Iteration 1 - using callbacks
 // ...
+getInstruction('mashedPotatoes', 0, (step0) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
+  getInstruction('mashedPotatoes', 1, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+    getInstruction('mashedPotatoes', 2, (step2) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+      getInstruction('mashedPotatoes', 3, (step3) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+        getInstruction('mashedPotatoes', 4, (step4) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
+        }, (err) => console.log(err));
+      }, (err) => console.log(err));
+    }, (err) => console.log(err));
+  }, (err) => console.log(err));
+}, (err) => console.log(err));
 
 // Iteration 2 - using promises
 // ...
-
+obtainInstruction('steak', 0)
+  .then((step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
+    return obtainInstruction('steak', 1);
+  })
+  .then((step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
+    return obtainInstruction('steak', 2);
+  })
+  .then((step2) => {
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
+    return obtainInstruction('steak', 3);
+  })
+  .then((step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
+    return obtainInstruction('steak', 4);
+  })
+  .then((step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
+    document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
+ 
+    // Bonus Iteration 4: mostrar imagen del steak
+    document.querySelector("#steak-img").removeAttribute("hidden");
+  })
+  .catch((err) => console.log(err));
 // Iteration 3 using async/await
 // ...
-
+async function makeBroccoli() {
+  try {
+    const step0 = await obtainInstruction('broccoli', 0);
+    document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+ 
+    const step1 = await obtainInstruction('broccoli', 1);
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+ 
+    const step2 = await obtainInstruction('broccoli', 2);
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+ 
+    const step3 = await obtainInstruction('broccoli', 3);
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+ 
+    const step4 = await obtainInstruction('broccoli', 4);
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+ 
+    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+ 
+    // Bonus Iteration 4: mostrar imagen del brócoli
+    document.querySelector("#broccoli-img").removeAttribute("hidden");
+  } catch (err) {
+    console.log(err);
+  }
+}
+ 
+makeBroccoli();
 // Bonus 2 - Promise all
 // ...
